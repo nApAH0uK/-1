3b474fab419c8400b7804508878c17e2fe195f19
nApAH0uK <jojo_o_mali@mail.ru>
2025-10-17 01:11:43 +0500

    Добавлен класс Factory с валидацией и методом NameSamsung

---
+++ Labaratorka_Yarmingin_BPI-23-02/Factory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaratorka_Yarmingin_BPI_23_02
+{
+    class Factory
+    {
+    }
+}
