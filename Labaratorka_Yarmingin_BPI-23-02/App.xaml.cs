68bb87a43202717b8fe5b4837a6519e159529d7c
nApAH0uK <jojo_o_mali@mail.ru>
2025-10-16 22:23:54 +0500

    Add C# WPF project: solution, source code and proper gitignore

---
+++ Labaratorka_Yarmingin_BPI-23-02/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Labaratorka_Yarmingin_BPI_23_02;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
