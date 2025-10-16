ed37af712e45c215e4adbfd37b95af04413ebbc3
nApAH0uK <jojo_o_mali@mail.ru>
2025-10-16 23:58:53 +0500

    Добавлены темы оформления
    - Реализована темная тема (Dark.xaml)
    - Реализована светлая тема (Light.xaml)
    - Добавлены кнопки переключения между темами
    - Обновлены стили интерфейса
    - Добавлен .gitignore для исключения временных файлов

--- Labaratorka_Yarmingin_BPI-23-02/MainWindow.xaml.cs
+++ Labaratorka_Yarmingin_BPI-23-02/MainWindow.xaml.cs
@@ -19,5 +19,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        var uri = new Uri(@"Dark.xaml", UriKind.Relative);
+        ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+        Application.Current.Resources.Clear();
+        Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+    }
+    private void DarkButton_Click(object sender, RoutedEventArgs e)
+    {
+        var uri = new Uri(@"Dark.xaml", UriKind.Relative);
+        ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+        Application.Current.Resources.Clear();
+        Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+    }
+    private void LightButton_Click(object sender, RoutedEventArgs e)
+    {
+        var uri = new Uri(@"Light.xaml", UriKind.Relative);
+        ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+        Application.Current.Resources.Clear();
+        Application.Current.Resources.MergedDictionaries.Add(resourceDict);
     }
 }
